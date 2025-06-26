25521aa5ddb26c279069e19efd5d67eba03541a7
sanikasathe <sanikasathe32@gmail.com>
2025-06-26 11:53:23 +0530

    Create  Even_Odd.java

---
+++ Even_Odd.java
@@ -0,0 +1,12 @@
+package com.dkte;
+
+public class Even_Odd {
+    public static void main(String[] args) {
+        int a = 15;
+        if (a % 2 == 0) {
+            System.out.println(a+" is even number");
+        } else {
+            System.out.println(a+" is odd number");
+        }
+    }
+}
