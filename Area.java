5aeda8bd1aefe6b05366642e39a7cd88bd5e0e57
sanikasathe <sanikasathe32@gmail.com>
2025-06-30 12:51:26 +0530

    Create Area.java

---
+++ Area.java
@@ -0,0 +1,21 @@
+package com.dkte;
+import java.util.Scanner;
+
+public class Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.print("length");
+		int len=sc.nextInt();
+		System.out.print("bredth");
+		int bred=sc.nextInt();
+		int area=len*bred;
+		System.out.print("area" +area);
+		
+		
+		
+
+	}
+
+}
