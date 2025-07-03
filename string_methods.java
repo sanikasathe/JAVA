e0375b461daa8ea90a98c5f0d553f1515742f1c1
sanikasathe <sanikasathe32@gmail.com>
2025-07-03 19:49:26 +0530

    Create string_methods.java

---
+++ string_methods.java
@@ -0,0 +1,52 @@
+package com.dkte;
+
+public class string_methods {
+	//************************length() returns number of chars******************************************************
+	//*********************** charAt() returns char at given index -- 0 to length()-1**********************************
+
+	/*public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str="Sunbeam";
+		System.out.println("length"+str.length());
+		for(int i=0;i<str.length();i++)
+		{
+			char ch=str.charAt(i);
+			System.out.println(ch);
+
+		}
+
+	}*/
+	//********************************************indexof************************************************************************************
+	/*public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	String str1="hello world";
+	char ch='e';
+	int idx=str1.indexOf(ch);
+	System.out.println("char "+  ch+ " is at "+  idx+ " index");
+	
+	}*/
+	//*************************************use of uppercase&lowercase*****************************************************
+	/*public static void main(String[] args) {
+	// TODO Auto-generated method stub
+		String str2="hello world";
+		String str3=str2.toUpperCase();
+		System.out.println(str3);
+		String str4=str2.toLowerCase();
+		System.out.println(str4);
+
+}*/
+	//------------------------------------concatanation-----------------------------------------------------------------
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			String str3="Sanika";
+			String str4="Sathe";
+			String str5=str3.concat(str4);
+			System.out.println(str5);
+
+	}
+	
+	
+
+
+}
