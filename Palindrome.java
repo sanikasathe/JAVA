1ac7f252daf0d3021acad5118281a549bf58e1fe
sanikasathe <sanikasathe32@gmail.com>
2025-07-07 19:06:00 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,34 @@
+package com.dkte;
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.print("enter the string");
+		String Original_String=sc.nextLine();
+		StringBuilder sb1 = new StringBuilder(Original_String); 
+
+		String rev_str = sb1.reverse().toString(); 
+		System.out.println("original string "+ Original_String);
+		System.out.println("reversed string "+ rev_str);
+		if( Original_String.equals(rev_str))
+		{
+			System.out.println("string is palindrome");
+			
+		}
+		else
+		{
+			System.out.println("string is not palindrome");
+
+		}
+		
+		
+		
+
+		
+
+	}
+
+}
