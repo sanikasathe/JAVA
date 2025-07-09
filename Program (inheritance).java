9b23f6a4d181d770f636c898eca1f7b5d35cbbde
sanikasathe <sanikasathe32@gmail.com>
2025-07-09 10:53:46 +0530

    Create  Program (inheritance).java

---
+++ Program (inheritance).java
@@ -0,0 +1,21 @@
+package com.sunbeam;
+class Parent{
+	public final void display( ) { // logically 100% complete 
+		//TODO 
+	}
+}
+class Child extends Parent{
+	/*  // final method cannot be overrided 
+	@Override
+	public void display() {
+		//TODO 
+	}*/ 
+}
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
